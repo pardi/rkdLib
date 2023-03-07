1ccb2c3f12ffa04cbfe98b3674aa623e0b3ef39f
pardi <tommaso.pardi@gmail.com>
2023-03-07 00:37:34 +0000

    feat{example}: made a more meaningfull example

--- examples/example_init_class.cpp
+++ examples/example_init_class.cpp
@@ -2,7 +2,14 @@
 
 int main(){
 
-	RKD::Robot r;
+	RKD::Robot panda_robot("../urdf/panda.urdf", "panda_link0", "panda_hand");
 	
+	Eigen::VectorXd q(7);
+	q << 0, 0, 0, 0, 0, 0, 0;
+
+	Eigen::VectorXd p = panda_robot.getFK(q);
+
+	std::cout << p << std::endl;
+
 	return 0;
 }
